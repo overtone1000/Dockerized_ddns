6c256f897803c6ac81131219fe3bbca09139859e
Tyler Moore <TylerRMoore@gmail.com>
2024-09-29 15:57:19 -0700

    Need to own config file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+FILE=/etc/ddclient/ddclient.conf
+USER=$(whoami)
+chown $USER $FILE 
+
 while :
 do
     echo "Starting ddclient in foreground"
-    RESULT=$(ddclient -foreground -noquiet -verbose -debug -file /etc/ddclient/ddclient.conf)
-    echo "Exited with result $RESULT. Restarting in 5 seconds."
+    RESULT=$(ddclient -foreground -noquiet -verbose -debug -file $FILE)
+    echo "Exited with result $RESULT. Restarting in 60 seconds."
     sleep 60
 done
